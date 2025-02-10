23766b48b97ea2b5fa702f8c86df446f14d2e915
Bhavesh Mishra <130285034+bhavesh0907@users.noreply.github.com>
2025-02-11 03:52:31 +0530

    drone_control.cpp

---
+++ drone_control.cpp
@@ -0,0 +1,10 @@
+#include "drone_control.h"
+#include <iostream>
+
+void initializeDrone() {
+    std::cout << "Initializing drone...\n";
+}
+
+void moveDrone(std::vector<std::pair<int, int>> path) {
+    std::cout << "Moving drone along planned path...\n";
+}
